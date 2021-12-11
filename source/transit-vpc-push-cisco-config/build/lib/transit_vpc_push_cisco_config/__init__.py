75af1fe665def77a8d217f2878694ffc8fdd0699
Ubuntu <ubuntu@ip-10-0-0-91.ap-southeast-1.compute.internal>
2021-12-11 07:59:25 +0000

    erase akk

--- source/transit-vpc-push-cisco-config/build/lib/transit_vpc_push_cisco_config/__init__.py
+++
@@ -1,4 +0,0 @@
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('transit-vpc-push-cisco-config').version
-__release_date__ = "Aug-2017"
--- source/transit-vpc-solution-helper/build/lib/transit_vpc_solution_helper/__init__.py
+++
@@ -1,4 +0,0 @@
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('transit-vpc-solution-helper').version
-__release_date__ = "Apr-2019"
